d24c3b1cdd7dc4ba7edf3d10412434a4ca35003b
Ranjith kumar <77315416+Ranjithkumar-code@users.noreply.github.com>
2021-01-12 12:07:49 +0530

    Add files via upload

---
+++ Actions.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+	<title>HOME PAGE</title>
+</head>
+<body>
+	<div class="hero">
+		<form>
+		<nav>
+			<img src="img/logo.png" class="logo">
+			<ul>
+				<li><a href="home.php">Home</a></li>
+				<li><a href="">Action</a></li>
+				<li><a href="">About us</a></li>
+				<li><button type="submit" name="logout" class="logout">Logout</button></li>
+			</ul>
+		</nav>
+		</form>
+		<form>
+			<div class="action-btn">
+				<ul>
+					<li><a href="">ADD DATA</a></li>
+					<li><a href="">LIST DATA</a></li>
+				</ul>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+	<title>HOME PAGE</title>
+</head>
+<body>
+	<div class="hero">
+		<form >
+		<nav>
+			<img src="logo.png" class="logo">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="Actions.php">Action</a></li>
+				<li><a href="">About us</a></li>
+				<li><button type="submit" name="logout" class="logout">Logout</button></li>
+			</ul>
+		</nav>
+	</form>
+		<div class="info">
+			<h1>I'm <span>Ranjith</span> Kumar</h1>
+			<p>This is my official website to showcase my all works related to web<br> development and UI design.</p>
+			<a href="">Download</a>
+		</div>
+		<div class="img-box">
+			<img src="back.png" class="img-back">
+			<img src="rk.png" class="img-main">
+		</div>
+		<div class="social-link">
+			<a href=""><i class="fab fa-facebook-square"></i></a>
+			<a href=""><i class="fab fa-linkedin"></i></a>
+			<a href=""><i class="fab fa-whatsapp-square"></i></a>
+			<a href=""><i class="fab fa-instagram-square"></i></a>
+		</div>
+	</div>
+</body>
+</html>
